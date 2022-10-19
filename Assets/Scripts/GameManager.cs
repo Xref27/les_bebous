bb77e4424047b9cea000d62dff16e93da6d1ed4d
FidelGastro <86535203+FidelsGastro@users.noreply.github.com>
2022-10-19 23:49:21 +0200

    Menu Pause

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject Pause;
+    public bool IsPause;
+
     public void quit()
     {
         Application.Quit();
         Debug.Log("le jeu est quitté bg fais moi confiance");
     }
 
-    public void MyLoadScene()
+    public void LoadGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
         Time.timeScale = 1;
+    }
+
+    public void SetPause(bool value)
+    {
+        Pause.SetActive(value);
+        IsPause = value;
+
+        if (value)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+
+        Debug.Log(Time.timeScale);
     }
 }
