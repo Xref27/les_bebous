9abe2ea161a71d8391c12a06bd6f6053826fe0f7
iamkeksi <113089990+iamkeksi@users.noreply.github.com>
2022-10-20 11:56:28 +0200

    added player mvm + deadzone

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
+    public static PlayerMovement Instance { get; private set; }
+
+    private Rigidbody2D rb;
+    private int _canJump = 2;
+
+    public int jumpForce;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    void Update()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * 5f, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_canJump > 0)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                _canJump--;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _canJump = 2;
+        }
     }
 }
