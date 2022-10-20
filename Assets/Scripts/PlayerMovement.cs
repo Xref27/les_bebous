9b48eadd34cfbe50b0879db13123c113ef9e53be
Alvi Frémont <alvi.fremont@edu.devinci.fr>
2022-10-20 23:00:44 +0200

    added sfx, + bg music + fixed tile colliders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,8 @@ public class PlayerMovement : MonoBehaviour
     private int _canJump = 2;
 
     public int jumpForce;
-
+    public AudioSource run;
+    public AudioSource jumpsound;
     private void Awake()
     {
         Instance = this;
@@ -19,20 +20,24 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
 
+    }
 
 
     private void Update()
     {
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * 5f, rb.velocity.y);
-
+        if(Mathf.Abs(rb.velocity.x)>0.1f&& Mathf.Abs(rb.velocity.y) < 0.1f)
+        {
+            run.Play();
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (_canJump > 0)
             {
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
                 _canJump--;
+                jumpsound.Play();
             }
         }
     }
